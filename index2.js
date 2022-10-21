20ae4b5244120c278f0f09dfcabdb92e2f2cbf5c
nitingupta5856 <nitingupta5856>
2022-10-21 12:02:43 +0530

    final updated

--- index2.js
+++ index2.js
@@ -41,18 +41,87 @@
 // third.style.visibility  ='hidden';
 
 // query selector all
-var titles =document.querySelectorAll('.title');
-console.log(titles);
-titles[0].textContent='hello';
-var even = document.querySelector('li:nth-child(2)')
-console.log(even);
- even.style.color='green';
+// var titles =document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent='hello';
+// var even = document.querySelector('li:nth-child(2)')
+// console.log(even);
+//  even.style.color='green';
 
 
-var odd = document.querySelectorAll('li:nth-child(odd)')
+// var odd = document.querySelectorAll('li:nth-child(odd)')
 
-for(var i=0;i<odd.length;i++){
-    odd[i].style.background='green';
-}
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.background='green';
+// }
+
+
 
 // querySelector() methodcan only be used to access a single element while querySelectorAll() method can be used to access all elements which match with a specified CSS selector
+// parentNode
+// var itemlist =document.querySelector('#items');
+// console.log(itemlist.parentNode);
+
+// itemlist.parentNode.style.background="blue";
+// console.log(itemlist.parentNode.parentNode);
+// parentElement
+var itemlist =document.querySelector('#items');
+// console.log(itemlist.parentElement);
+
+// itemlist.parentElement.style.background="blue";
+// console.log(itemlist.parentElement.parentElement.parentElement);
+
+// ChildNode
+// console.log(itemlist.childNodes);
+// console.log(itemlist.children);
+// console.log(itemlist.children[1]);
+// itemlist.children[1].style.background="yellow";
+
+// first child
+// console.log(itemlist.firstChild);
+// first element child
+// console.log(itemlist.firstElementChild);
+
+// itemlist.firstElementChild.textContent='hello';
+// last child
+// console.log(itemlist.lastChild);
+// last element child
+// console.log(itemlist.lastElementChild);
+// itemlist.lastElementChild.textContent='hello';
+// next sibling
+// console.log(itemlist.nextSibling);
+// next element sibling
+// console.log(itemlist.nextElementSibling);
+// previous sibling
+// console.log(itemlist.previousSibling);
+// previous element sibling
+// console.log(itemlist.previousElementSibling);
+// itemlist.previousElementSibling.style.color='green';
+// create element
+var newDiv=document.createElement('div');
+// console.log(newDiv);
+// add class
+newDiv.className="updated";
+// console.log(newDiv);
+// add id
+
+newDiv.id="updatedjj";
+// console.log(newDiv);
+// add atribute
+newDiv.setAttribute('title', 'Hello Div');
+console.log(newDiv);
+// CREATE Text node
+var newtextdiv= document.createTextNode('hello world');
+// add text node
+newDiv.appendChild(newtextdiv);
+console.log(newDiv);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container.insertBefore(newDiv, h1);
+newDiv.style.fontSize='30px';
+
+var parent =document.querySelector('.list-group');
+console.log(parent.innerHTML);
+// parent.innerHTML
+parent.innerHTML='<li>Hello world'+ parent.innerHTML;
